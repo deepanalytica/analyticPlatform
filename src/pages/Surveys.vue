e20f05e5a9f0bc34678e6a676fe4e700a5aa5d6d
alexsavedra <deepanalytica@gmail.com>
2022-04-30 13:44:14 -0400

    add btn surveys

--- src/pages/Surveys.vue
+++ src/pages/Surveys.vue
@@ -1,4 +1,30 @@
 <template>
-<h2>Formulario</h2>
+  <q-page padding>
+    <h2>Formulario</h2>
 
+    <q-form class="row q-col-gutter-md" >
+
+      <div class="col-12 col-sm-6">
+
+        <q-input
+          label="Nombre"
+        />
+
+      </div>
+
+      <div class="col-12">
+            <q-btn
+            label="Submit"
+            color="primary"
+            />
+            <q-btn
+                  label="Reset"
+                  color="primary"
+                  outline
+                  class="q-ml-sm"
+                  :rippler="false"
+            />
+      </div>
+    </q-form>
+  </q-page>
 </template>
